435f52536439b380159ef40241a698ec382bbef1
liuenci <154910381@qq.com>
2019-01-20 20:22:11 +0800

    download

--- liuencier-security-demo/src/main/java/com/liuencier/web/controller/FileController.java
+++ liuencier-security-demo/src/main/java/com/liuencier/web/controller/FileController.java
@@ -1,13 +1,13 @@
 package com.liuencier.web.controller;
 
 import com.liuencier.dto.FileInfo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.io.IOUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.Date;
 
 @RestController
@@ -27,5 +27,19 @@ public class FileController {
         file.transferTo(localFile);
 
         return new FileInfo(localFile.getAbsolutePath());
+    }
+
+    @GetMapping("{id}")
+    public void download(@PathVariable String id, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try (
+                InputStream inputStream = new FileInputStream(new File(folder, id + ".txt"));
+                OutputStream outputStream = response.getOutputStream();
+        ) {
+            response.setContentType("application/x-download");
+            response.addHeader("Content-Disposition", "attachment;filename=1.jpg");
+
+            IOUtils.copy(inputStream, outputStream);
+            outputStream.flush();
+        }
     }
 }
